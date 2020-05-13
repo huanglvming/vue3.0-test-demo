856afdd0ed203b1d762ec6ae04b9502cb351a0bf
well <well@meishakeji.com>
2020-05-13 20:20:18 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <button @click="click">{{ state.count }}</button>
+  </div>
+</template>
+
+<script>
+import { reactive, computed } from 'vue'
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0
+    });
+    function click() {
+      state.count += 1;
+    }
+    return {
+      state,
+      click
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
